9bc2584d1e3149e438f5f7fe44f10b2697fdc006
conuko <constantinu93@gmail.com>
2022-12-08 13:31:44 +0100

    feat: add solution for day6 part2

---
+++ src/2022/6/2.ts
@@ -0,0 +1,26 @@
+import fs from 'fs';
+
+const input = fs.readFileSync('input.txt', 'utf8');
+const inputArray = input.split('');
+let answer = 0;
+const marker = 14;
+
+for (let i = marker; i < inputArray.length; i++) {
+	const buffer = inputArray.slice(i - marker, i);
+	console.log(buffer.join(''));
+	if (allCharsUnique(buffer.join(''))) {
+		answer = i;
+		break;
+	}
+}
+
+function allCharsUnique(str: string) {
+	for (let i = 0; i < str.length; i++) {
+		if (str.indexOf(str[i]) !== i) {
+			return false;
+		}
+	}
+	return true;
+}
+
+console.log(answer);
