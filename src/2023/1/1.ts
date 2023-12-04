2f632652a4fadcf9b941b4889959f7e80de9cd25
Constantin Unterkofler <50672977+conuko@users.noreply.github.com>
2023-12-04 09:31:16 +0100

    feat(2023/1): add solution for part 1

---
+++ src/2023/1/1.ts
@@ -0,0 +1,32 @@
+import fs from 'fs';
+
+const input = fs.readFileSync('input.txt', 'utf8');
+const inputArray = input.split('\n');
+
+const isDigit = (str: string) => {
+	return /^\d$/.test(str);
+};
+
+const getResultNumbers = (inputArray: string[]) => {
+	const numbers: number[] = [];
+
+	inputArray.forEach((line) => {
+		const firstDigit = line.split('').find((word) => isDigit(word));
+
+		const secondDigit = line
+			.split('')
+			.reverse()
+			.find((word) => isDigit(word));
+
+		const result = parseInt(`${firstDigit}${secondDigit}`);
+
+		!Number.isNaN(result) && numbers.push(result);
+	});
+
+	console.log(numbers);
+	return numbers;
+};
+
+const result = getResultNumbers(inputArray).reduce((a, b) => a + b, 0);
+
+console.log(result);
